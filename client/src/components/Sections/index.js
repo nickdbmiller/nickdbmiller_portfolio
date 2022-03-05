e10dc6915e31afaf4fbc940107523bc2eb165a22
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-05 11:22:29 -0500

    Set default export for all sections.

---
+++ client/src/components/Sections/index.js
@@ -0,0 +1,6 @@
+// Sets default export for all sections.
+export { default as Contact } from "./Contact";
+export { default as Intro } from "./Intro";
+export { default as Projects } from "./Projects";
+export { default as Resume } from "./Resume";
+export { default as Skills } from "./Skills";
