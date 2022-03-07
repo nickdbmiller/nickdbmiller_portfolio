8129d0c9a80d35257169d16db0e2c1ded7405b40
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-07 16:00:35 -0500

    Add downloadable PDF.

--- client/src/components/Sections/Resume.jsx
+++ client/src/components/Sections/Resume.jsx
@@ -1,14 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
+import pdf from "../../assets/pdf/NICK_MILLER_ResumeBAY_2022.pdf"
 
 export default function Resume() {
     return (
-        <section
-            className="bg-lime-900 text-lime-100 rounded-2xl p-3 text-center"
+        <a
+            href={pdf}
+            target="_blank"
+            rel='noreferrer'
         >
-            <p>{'"Dowload my resume!"'}</p>
-            <FontAwesomeIcon icon={faArrowRightToBracket} className="rotate-90"/>
-        </section>
+            <section
+                className="bg-lime-900 text-lime-100 rounded-2xl p-3 text-center text-2xl"
+            >
+                <p>{'"Dowload my resume!"'}</p>
+                <FontAwesomeIcon icon={faArrowRightToBracket} className="rotate-90"/>
+            </section>
+        </a>
     )
 }
