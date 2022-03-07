88eee5defa16d212b7d9219d5d06d7d45352eb67
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-07 15:14:51 -0500

    Composible info elements.

---
+++ client/src/components/SkillCard.jsx
@@ -0,0 +1,9 @@
+export default function SkillCard(props) {
+    return (
+        <div
+            className="bg-lime-100 rounded-2xl p-3"
+        >
+            <h3>{props.title}</h3>
+        </div>
+    )
+}
