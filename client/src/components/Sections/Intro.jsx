82fdab099f1283e867a502e49313733060a20916
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-07 15:49:10 -0500

    Add professional title.

--- client/src/components/Sections/Intro.jsx
+++ client/src/components/Sections/Intro.jsx
@@ -3,17 +3,26 @@ import ConversationBubble from "../ConversationBubble";
 export default function Intro() {
     return (
         <section
-            className="flex flex-row"
+            className="flex flex-col items-center"
         >
-            <img
-                src="https://i.imgur.com/u0yC6n7.jpg"
-                alt="Headshot of Nick Miller."
-                className="rounded-2xl h-auto w-40"
-            />
-            <ConversationBubble
-                text = {'"Hi, I\'m Nick! Making things simple is what I do."'}
-                className = "bg-lime-900 text-lime-100 rounded-2xl p-3"
-            />
+            <div
+                className="flex flex-row space-x-4"
+            >
+                <img
+                    src="https://i.imgur.com/u0yC6n7.jpg"
+                    alt="Headshot of Nick Miller."
+                    className="rounded-2xl h-auto w-40"
+                />
+                <ConversationBubble
+                    text = {'"Hi, I\'m Nick! Making things simple is what I do."'}
+                    className = "bg-lime-900 text-lime-100 text-2xl rounded-2xl p-3"
+                />
+            </div>
+            <h2
+                className="text-xl mt-6 text-lime-1000"
+            >
+                Full Stack Software Engineer
+            </h2>
         </section>
     )
 }
