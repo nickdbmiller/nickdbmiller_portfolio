e3f94a1937fddf4df701d0ae19442478b01cf2c2
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-08 14:10:33 -0500

    Fix aspect ratio.

--- client/src/components/Sections/Intro.jsx
+++ client/src/components/Sections/Intro.jsx
@@ -3,19 +3,19 @@ import ConversationBubble from "../ConversationBubble";
 export default function Intro() {
     return (
         <section
-            className="flex flex-col items-center"
+            className="flex flex-col items-center max-w-md"
         >
             <div
-                className="flex flex-row space-x-4"
+                className="flex flex-row space-x-4 items-center justify-center"
             >
                 <img
                     src="https://i.imgur.com/u0yC6n7.jpg"
                     alt="Headshot of Nick Miller."
-                    className="rounded-2xl h-auto w-40"
+                    className="rounded-2xl w-40"
                 />
                 <ConversationBubble
                     text = {'"Hi, I\'m Nick! Making things simple is what I do."'}
-                    className = "bg-lime-900 text-lime-100 text-2xl rounded-2xl p-3"
+                    className = "bg-lime-900 text-lime-100 text-2xl rounded-2xl p-3 sm:text-4xl flex items-center"
                 />
             </div>
             <h2
