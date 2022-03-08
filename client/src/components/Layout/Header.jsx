a30b614a883bfe48254ff8c88c99ba10abb64caf
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-08 14:10:11 -0500

    Center header items on large screens.

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import LinkIcons from "../LinkIcons";
 export default function Header() {
     return (
         <header
-            className="bg-lime-700 text-lime-1000 flex flex-row justify-between items-center p-3
+            className="bg-lime-700 text-lime-1000 flex flex-row justify-between sm:justify-evenly items-center p-3 2xl:justify-center 2xl:space-x-40
             sticky top-0 z-50"
         >
             <h1
