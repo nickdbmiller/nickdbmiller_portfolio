25baec3fee074ac9493b6704dd0b1a375533bc4a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-09 09:54:02 -0500

    Add modal toggle.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
+import { useState } from 'react'
+
 import Layout from './components/Layout/Layout';
 import { Contact, Intro, Projects, Resume, Skills } from './components/Sections'
 
 import './App.css';
 
 function App() {
+  const [modalToggle, setModalToggle] = useState(false)
+
   return (
     <div className="font-raleway">
-      <Layout>
+      <Layout
+        modalToggle = {modalToggle}
+        setModalToggle = {setModalToggle}
+      >
         <Intro />
         <Projects />
         <Skills />
         <Resume />
-        <Contact />
+        <Contact
+          modalToggle = {modalToggle}
+          setModalToggle = {setModalToggle}
+        />
       </Layout>
     </div>
   );
