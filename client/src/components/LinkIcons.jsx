b0a5b4ce361f26e3977cb9e8398cd7df1875b559
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-09 09:54:45 -0500

    Fix button animations.

--- client/src/components/LinkIcons.jsx
+++ client/src/components/LinkIcons.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons"
 import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-export default function LinkIcons() {
+export default function LinkIcons(props) {
     return (
         <div
             className='space-x-4 text-4xl'
@@ -11,26 +11,28 @@ export default function LinkIcons() {
                 href='https://www.linkedin.com/in/ndbmiller'
                 target='_blank'
                 rel='noreferrer'
-                className='hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500 hover:text-lime-300'
-            >
-                <FontAwesomeIcon icon={faLinkedin} />
+                >
+                <FontAwesomeIcon
+                    className='hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500 hover:text-lime-300'
+                    icon={faLinkedin}
+                />
             </a>
             <a
                 href='https://github.com/nickdbmiller'
                 target='_blank'
                 rel='noreferrer'
-                className=' hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500 hover:text-lime-300'
-            >
-                <FontAwesomeIcon icon={faGithubSquare} />
+                >
+                <FontAwesomeIcon
+                    className=' hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500 hover:text-lime-300'
+                    icon={faGithubSquare}
+                />
             </a>
-            <a
-                href="mailto:nickbarrettmiller@gmail.com?subject=Nice Portfolio!&body=Hi Nick,"
-                target='_blank'
-                rel='noreferrer'
+            <button
+                onClick={() => props.setModalToggle(!props.modalToggle)}
                 className=' hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50 active:text-lime-500 hover:text-lime-300'
             >
                 <FontAwesomeIcon icon={faSquareEnvelope} />
-            </a>
+            </button>
         </div>
     )
 }
