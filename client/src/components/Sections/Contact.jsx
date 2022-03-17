14d96065965d908e8c02e5b852143cb3885fbfc2
nickdbmiller <nickbarrettmiller@gmail.com>
2022-03-17 15:51:18 -0400

    Remove quotes.

--- client/src/components/Sections/Contact.jsx
+++ client/src/components/Sections/Contact.jsx
@@ -10,11 +10,11 @@ export default function Contact(props) {
                 className="flex flex-row space-x-4 hover:-translate-y-2 transform transition focus:outline-none focus:ring focus:ring-offset-2 focus:ring-lime-400 focus:ring-opacity-50"
             >
                 <ConversationBubble
-                    text = {'Click here to get in touch.'}
+                    text = "Click here to get in touch."
                     className = "bg-lime-100 text-lime-1000 rounded-2xl p-3 active:text-lime-500 hover:text-lime-300 hover:bg-lime-1000"
                 />
                 <ConversationBubble
-                    text = {'"Let\'s see how I can make your problems simple!"'}
+                    text = "Let's see how I can make your problems simple!"
                     className = "bg-lime-900 text-lime-100 rounded-2xl p-3 active:text-lime-500 hover:text-lime-300 hover:bg-lime-1000"
                 />
             </button>
